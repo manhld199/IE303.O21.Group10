4aca92564e9ce1b6f814044c7f22304c32a07b8a
namtuthien <leducmanh123kt@gmail.com>
2024-04-23 23:52:31 +0700

    add: AdminDashboardPage

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,10 @@
+// import libs
+import React from "react";
+
+export default function CustomerLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
